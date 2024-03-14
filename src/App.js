1769bf8fa618dc4c2a33937578ef88e5318a8c7c
ashley burns <burns.ashley331@gmail.com>
2024-03-14 10:21:35 -0400

    Completed hands on lab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const currDate = new Date();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ashley Burns</h1>
+      <h2>It is {currDate.toLocaleDateString()} and the time is {currDate.toLocaleTimeString()}.</h2>
     </div>
   );
 }
